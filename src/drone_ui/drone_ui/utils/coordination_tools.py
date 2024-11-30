0880945315f0bc4b3c92bff7499e678d862efac9
albud187 <alfab@MSI.localdomain>
2024-11-30 11:38:24 -0500

    begin working on attack task

--- src/drone_ui/drone_ui/utils/coordination_tools.py
+++ src/drone_ui/drone_ui/utils/coordination_tools.py
@@ -2,59 +2,46 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Vector3, PoseStamped
+from scipy.optimize import linear_sum_assignment
+import numpy as np
 
 
 def coordinated_movement_goals(pg_node, goal_pose):
     all_friendly_positions = pg_node.friendly_drones_positions
-    if len(pg_node.selected_drones) > 1:
-        
+    selected_drones = pg_node.selected_drones
 
-        centroid_x = 0
-        centroid_y = 0
-        centroid_z = 0
+    if len(selected_drones) > 1:
+        # Extract positions of selected drones
+        positions = np.array([all_friendly_positions[r]["sim"] for r in selected_drones])
 
-        # Get centroid of drones current_positions
-        for r in pg_node.selected_drones:
-            centroid_x += all_friendly_positions[r]["sim"][0]
-            centroid_y += all_friendly_positions[r]["sim"][1]
-            centroid_z += all_friendly_positions[r]["sim"][2]
+        # Calculate centroid of the selected drones
+        centroid = np.mean(positions, axis=0)
 
-        centroid_x = centroid_x / len(pg_node.selected_drones)
-        centroid_y = centroid_y / len(pg_node.selected_drones)
-        centroid_z = centroid_z / len(pg_node.selected_drones)
+        # Calculate displacements from centroid
+        displacements = positions - centroid
 
-        d_pos_fr = {}
-
-        # Get displacements between centroid and current drone positions
-        for r in pg_node.selected_drones:
-            dx = all_friendly_positions[r]["sim"][0] - centroid_x
-            dy = all_friendly_positions[r]["sim"][1] - centroid_y
-            dz = all_friendly_positions[r]["sim"][2] - centroid_z
-
-            d_pos_fr[r] = (dx, dy, dz)
-
+        # Generate goal poses
         goal_poses = {}
-        for r in pg_node.selected_drones:
-            gp_x = goal_pose[0] + d_pos_fr[r][0]
-            gp_y = goal_pose[1] + d_pos_fr[r][1]
-            gp_z = centroid_z
+        for r, displacement in zip(selected_drones, displacements):
+            goal_x, goal_y = goal_pose[:2] + displacement[:2]
+            goal_z = centroid[2]  # Maintain centroid altitude
 
             drone_goal = PoseStamped()
             drone_goal.header.frame_id = "1"
-            drone_goal.pose.position.x = gp_x
-            drone_goal.pose.position.y = gp_y
-            drone_goal.pose.position.z = gp_z + 1
+            drone_goal.pose.position.x = goal_x
+            drone_goal.pose.position.y = goal_y
+            drone_goal.pose.position.z = goal_z
 
             goal_poses[r] = drone_goal
 
     else:  # Case for a single drone
         goal_poses = {}
-        for r in pg_node.selected_drones:
+        for r in selected_drones:
             drone_goal = PoseStamped()
             drone_goal.header.frame_id = "1"
             drone_goal.pose.position.x = goal_pose[0]
             drone_goal.pose.position.y = goal_pose[1]
-            drone_goal.pose.position.z = all_friendly_positions[r]["sim"][2] + 1
+            drone_goal.pose.position.z = all_friendly_positions[r]["sim"][2]
 
             goal_poses[r] = drone_goal
 
@@ -75,4 +62,16 @@ def set_hover_height(pg_node, hover_height):
     return goal_poses
 
 def coordinated_attack(pg_node):
+    attackers = pg_node.selected_drones
+    targets = pg_node.locked_targets
+
+
+    num_attackers = len(attackers)
+    num_targets = len(targets)
+
+    cost_matrix = np.zeros((num_attackers, num_targets))
+
+    attacker_positions = {}
+    target_positions = {}
+    #populate cost matrix with distances
     pass
